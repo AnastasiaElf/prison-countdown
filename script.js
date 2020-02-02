348d0e38e575c29097272227901ca06781eb31f6
Artemchuk Anastasia <artemchuk.nastya@gmail.com>
2020-02-02 23:12:17 +0300

    Add moment. Update result format.

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
 let countdownItem = document.getElementById("countdown");
+let totalItem = document.getElementById("total");
 let windowItem = document.getElementById("window");
-let bgItem = document.getElementById("background");
 
 const isOpenClass = "is-open";
 
-let startDate = new Date("Aug 13, 2019 00:00:00").getTime();
-let endDate = new Date("Aug 13, 2021 00:00:00").getTime();
+let startDate = moment("2019-08-13 00:00:00");
+let endDate = moment("2021-08-13 00:00:00");
 
-let x = setInterval(countTime, 1000);
+let interval = setInterval(updateTime, 1000);
 
-let countPoints = 30;
-let fullDistance = endDate - startDate;
-let distancePerPoint = fullDistance / countPoints;
+function updateTime() {
+    let now = moment();
 
-function countTime() {
-    
-    let now = new Date().getTime();
-    
-    let distance = endDate - now;
-    
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    let date = endDate.clone();
 
-    countdownItem.innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
+    let days = date.diff(now, 'days');
 
-    if (distance < 0) {
-        clearInterval(x);
+    let leftYears = date.diff(now, 'years');
+    date = date.subtract(leftYears, 'years');
+
+    let leftMonths = date.diff(now, 'months');
+    date = date.subtract(leftMonths, 'months');
+
+    let leftDays = date.diff(now, 'days');
+    date = date.subtract(leftDays, 'days');
+
+    let hours = date.diff(now, 'hours');
+    date = date.subtract(hours, 'hours');
+
+    let minutes = date.diff(now, 'minutes');
+    date = date.subtract(minutes, 'minutes');
+
+    let seconds = date.diff(now, 'seconds');
+
+    countdownItem.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    totalItem.innerHTML = leftYears + "y " + leftMonths + "m  " + leftDays + "d / 2y";
+
+    if (endDate.diff(now, 'seconds') < 0) {
+        clearInterval(interval);
         countdownItem.innerHTML = "0d 0h 0m 0s";
+        totalItem.innerHTML = "0y 0m 0d / 2y";
         windowItem.classList.add(isOpenClass);
     }
 }
 
-countTime();
+updateTime();
